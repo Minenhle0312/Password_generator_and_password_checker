4c8d8bc356de62098de7f758db5cba576b3d0abd
Que <mdlalose2021@gmail.com>
2026-09-11 12:08:43 +0200

    Integrated hashing for master password and master password verification to access the program

--- main.py
+++ main.py
@@ -1,7 +1,51 @@
 import random as rm
 import json
+import sys
+import os
+import hashlib
 
-FILENAME = "passwords.json"
+if getattr(sys, 'frozen', False):
+    # Running as a PyInstaller exe
+    APP_DIR = os.path.dirname(sys.executable)
+else:
+    # Running as a normal .py script
+    APP_DIR = os.path.dirname(os.path.abspath(__file__))
+
+FILENAME = os.path.join(APP_DIR, "passwords.json")
+
+#----------------------------------------------------------
+#create master password
+#----------------------------------------------------------
+def setup_master_password():
+    master_password = input("Enter your master passsword: ")
+    password_bytes = master_password.encode()
+    salt = os.urandom(16)
+    hashed_password = hashlib.pbkdf2_hmac(hash_name="sha256", password=password_bytes, salt=salt, iterations=100000, dklen=None)
+
+    master_data = {
+        "hash": hashed_password.hex(),
+        "salt": salt.hex()
+    }
+    save_data("master.json", master_data)
+    print("Master password set successfully.")
+
+#---------------------------------------------------------
+#verify master password
+#---------------------------------------------------------
+def verify_master_password():
+    master_data = load_data("master.json")
+    stored_hash = bytes.fromhex(master_data["hash"])
+    salt = bytes.fromhex(master_data["salt"])
+    master_password = input("Enter your master password to open the password manager: ")
+    password_bytes = master_password.encode()
+    hashed_password = hashlib.pbkdf2_hmac(hash_name="sha256", password=password_bytes, salt=salt, iterations=100000, dklen=None)
+
+    if hashed_password == stored_hash:
+        print("Opening password manager")
+        return True
+    else:
+        print("Master password incorrect")
+        return False
 
 print("======================================")
 print("=== Password checker and Generator ===")
@@ -90,30 +134,38 @@ def view_passwords():
         for service, password in data.items():
             print(f"{service}: {password}")
 
-#--------------Menu------------------
-while True:
-    print("1. Check Password")
-    print("2. Generate Password")
-    print("3. Save Password")
-    print("4. View Saved Passwords")
-    print("5. Exit")
-    choice = input("choose an option: ")
+if os.path.exists("master.json"):
+    verify_master_password()
+    access_granted = verify_master_password()
+else:
+    setup_master_password()
+    access_granted = True
 
-    #-------------Function selector------------------------------------------
-    if choice == "1":
-        password_check()
-    elif choice == "2":
-        password_generator()
-    elif choice == "3":
-        save_password()
-    elif choice == "4":
-        view_passwords()
-    elif choice == "5":
-        print("==========================================================")
-        print("====================Complete==============================")
-        print("==========================================================")
-        break
-    else:
-        print("Invalid option selected")
+if access_granted:
+#--------------Menu------------------
+    while True:
+        print("1. Check Password")
+        print("2. Generate Password")
+        print("3. Save Password")
+        print("4. View Saved Passwords")
+        print("5. Exit")
+        choice = input("choose an option: ")
+
+        #-------------Function selector------------------------------------------
+        if choice == "1":
+            password_check()
+        elif choice == "2":
+            password_generator()
+        elif choice == "3":
+            save_password()
+        elif choice == "4":
+            view_passwords()
+        elif choice == "5":
+            print("==========================================================")
+            print("====================Complete==============================")
+            print("==========================================================")
+            break
+        else:
+            print("Invalid option selected")
 
     print("==========================================================")
